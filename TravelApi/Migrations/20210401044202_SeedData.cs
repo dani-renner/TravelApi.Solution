6aaf24ae83ab894b0560f42917698851ebef9cec
Dani Renner <danijrenner@gmail.com>
2021-03-31 21:44:59 -0700

    add SeedData to migrations
    Co-authored-by: Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>

---
+++ TravelApi/Migrations/20210401044202_SeedData.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TravelApi.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Locations",
+                columns: new[] { "LocationId", "City", "Country" },
+                values: new object[,]
+                {
+                    { 1, "Bulawayo", "Zimbabwe" },
+                    { 2, "Rosario", "Argentina" },
+                    { 3, "Luxembourg", "Luxembourg" },
+                    { 4, "Bharatpur", "Nepal" },
+                    { 5, "Almaty", "Kazakhstan" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Reviews",
+                columns: new[] { "ReviewId", "LocationId", "Rating", "Text", "Title" },
+                values: new object[,]
+                {
+                    { 1, 1, 3, "Every day is comfortable and beautiful outside. But water supply is a big problem.", "Perfect Weather!" },
+                    { 2, 2, 3, "Every day is comfortable and beautiful outside. But there is crime and violence.", "Gorgeous Weather!" },
+                    { 3, 3, 2, "The rent is too damned high!", "Gloomy & Expensive!" },
+                    { 4, 4, 5, "There are wonderful flavors and spices here, and it's not too intense!", "Love the food!" },
+                    { 5, 5, 1, "The men here sure are not respectful to women!", "Ladies, don't go!" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Reviews",
+                keyColumn: "ReviewId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Reviews",
+                keyColumn: "ReviewId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Reviews",
+                keyColumn: "ReviewId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Reviews",
+                keyColumn: "ReviewId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Reviews",
+                keyColumn: "ReviewId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Locations",
+                keyColumn: "LocationId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Locations",
+                keyColumn: "LocationId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Locations",
+                keyColumn: "LocationId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Locations",
+                keyColumn: "LocationId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Locations",
+                keyColumn: "LocationId",
+                keyValue: 5);
+        }
+    }
+}
--- TravelApi/Models/TravelApiContext.cs
+++ TravelApi/Models/TravelApiContext.cs
@@ -2,14 +2,33 @@ using Microsoft.EntityFrameworkCore;
 
 namespace TravelApi.Models
 {
-    public class TravelApiContext : DbContext
+  public class TravelApiContext : DbContext
+  {
+    public TravelApiContext(DbContextOptions<TravelApiContext> options)
+        : base(options)
     {
-        public TravelApiContext(DbContextOptions<TravelApiContext> options)
-            : base(options)
-        {
-        }
+    }
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.Entity<Location>()
+      .HasData(
+        new Location { LocationId = 1, Country = "Zimbabwe", City = "Bulawayo" },
+        new Location { LocationId = 2, Country = "Argentina", City = "Rosario" },
+        new Location { LocationId = 3, Country = "Luxembourg", City = "Luxembourg" },
+        new Location { LocationId = 4, Country = "Nepal", City = "Bharatpur" },
+        new Location { LocationId = 5, Country = "Kazakhstan", City = "Almaty" }
+      );
+      builder.Entity<Review>()
+      .HasData(
+        new Review { ReviewId = 1, Rating = 3, Title = "Perfect Weather!", Text = "Every day is comfortable and beautiful outside. But water supply is a big problem.", LocationId = 1},
+        new Review { ReviewId = 2, Rating = 3, Title = "Gorgeous Weather!", Text = "Every day is comfortable and beautiful outside. But there is crime and violence.", LocationId = 2},
+        new Review { ReviewId = 3, Rating = 2, Title = "Gloomy & Expensive!", Text = "The rent is too damned high!", LocationId = 3},
+        new Review { ReviewId = 4, Rating = 5, Title = "Love the food!", Text = "There are wonderful flavors and spices here, and it's not too intense!", LocationId = 4},
+        new Review { ReviewId = 5, Rating = 1, Title = "Ladies, don't go!", Text = "The men here sure are not respectful to women!", LocationId = 5}
+      );
+    }
 
-        public DbSet<Location> Locations { get; set; }
-        public DbSet<Review> Reviews { get; set; }
-    } 
+    public DbSet<Location> Locations { get; set; }
+    public DbSet<Review> Reviews { get; set; }
+  } 
 }
